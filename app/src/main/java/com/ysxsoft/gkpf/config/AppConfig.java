20e349428b62298568a2ed290ef1a458ff82c8de
1475590636@qq.com <1475590636@qq.com>
2019-07-31 19:24:32 +0800

    1.测试

--- app/src/main/java/com/ysxsoft/gkpf/config/AppConfig.java
+++ app/src/main/java/com/ysxsoft/gkpf/config/AppConfig.java
@@ -2,6 +2,9 @@ package com.ysxsoft.gkpf.config;
 
 import android.os.Environment;
 
+/**
+ * 配置
+ */
 public class AppConfig {
     public static final String BASE_PATH = Environment.getExternalStorageDirectory() + "/ysxsoft/gkpf";
 }
