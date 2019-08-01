241c1c110283b4416507f84b2e8e5b4cb51c0d50
1475590636@qq.com <1475590636@qq.com>
2019-08-01 14:59:53 +0800

    1.注释test

--- app/src/main/java/com/ysxsoft/gkpf/bean/Test.java
+++ app/src/main/java/com/ysxsoft/gkpf/bean/Test.java
@@ -22,35 +22,35 @@ import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
-        List<CacheResponse.DataBean> cacheResponses=new ArrayList<>();
-        String cache="{\"groupId\":\"1\",\"requestId\":1,\"flowName1\":[{\"score\":1.5,\"isConfirmed\":false},{ \"score\":5,\"isConfirmed\":false}],\"flowName2\":[{\"score\":5,\"isConfirmed\":true},{\"score\":5, \"isConfirmed\":false}]}";
-        try {
-            JSONObject jsonObject=new JSONObject(cache);
-            Iterator<String> keys=jsonObject.keys();
-            while (keys.hasNext()){
-                String key=keys.next();
-                if ("groupId".equals(key)){
-                    String groupId=jsonObject.getString("groupId");
-                }else if ("requestId".equals(key)){
-                    int requestId=jsonObject.getInt("requestId");
-                }else {
-                    JSONArray array=jsonObject.getJSONArray(key);
-                    for (int i = 0; i <array.length() ; i++) {
-                        JSONObject object=array.getJSONObject(i);
-                        Object score=object.get("score");
-                        boolean isConfirmed=(boolean)object.get("isConfirmed");
-
-                        CacheResponse.DataBean response=new CacheResponse.DataBean();
-                        response.setConfirmed(isConfirmed);//是否确认
-                        response.setFlowName(key);//文件名
-                        response.setScore(score);//分数
-                        cacheResponses.add(response);
-                    }
-                }
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+//        List<CacheResponse.DataBean> cacheResponses=new ArrayList<>();
+//        String cache="{\"groupId\":\"1\",\"requestId\":1,\"flowName1\":[{\"score\":1.5,\"isConfirmed\":false},{ \"score\":5,\"isConfirmed\":false}],\"flowName2\":[{\"score\":5,\"isConfirmed\":true},{\"score\":5, \"isConfirmed\":false}]}";
+//        try {
+//            JSONObject jsonObject=new JSONObject(cache);
+//            Iterator<String> keys=jsonObject.keys();
+//            while (keys.hasNext()){
+//                String key=keys.next();
+//                if ("groupId".equals(key)){
+//                    String groupId=jsonObject.getString("groupId");
+//                }else if ("requestId".equals(key)){
+//                    int requestId=jsonObject.getInt("requestId");
+//                }else {
+//                    JSONArray array=jsonObject.getJSONArray(key);
+//                    for (int i = 0; i <array.length() ; i++) {
+//                        JSONObject object=array.getJSONObject(i);
+//                        Object score=object.get("score");
+//                        boolean isConfirmed=(boolean)object.get("isConfirmed");
+//
+//                        CacheResponse.DataBean response=new CacheResponse.DataBean();
+//                        response.setConfirmed(isConfirmed);//是否确认
+//                        response.setFlowName(key);//文件名
+//                        response.setScore(score);//分数
+//                        cacheResponses.add(response);
+//                    }
+//                }
+//            }
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
