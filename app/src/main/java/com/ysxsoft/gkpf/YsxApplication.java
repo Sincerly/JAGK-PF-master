82f454d00a1bb01fce62972447d1f61523267a77
Sincerly <1475590636@qq.com>
2019-08-02 13:54:40 +0800

    1.git提交测试

--- app/src/main/java/com/ysxsoft/gkpf/YsxApplication.java
+++ app/src/main/java/com/ysxsoft/gkpf/YsxApplication.java
@@ -2,6 +2,9 @@ package com.ysxsoft.gkpf;
 
 import android.support.multidex.MultiDexApplication;
 
+/**
+ * git配置测试
+ */
 public class YsxApplication extends MultiDexApplication {
 
     private static YsxApplication application;
